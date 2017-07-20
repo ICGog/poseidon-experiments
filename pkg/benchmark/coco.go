33a60b7206cdd9a82cc08031f9366fc41ed24108
Ionel Gog <gogionel@gmail.com>
2017-07-20 14:57:50 +0100

    Added synthetic pods.

--- pkg/benchmark/coco.go
+++ pkg/benchmark/coco.go
@@ -21,6 +21,7 @@ package benchmark
 import (
 	"fmt"
 	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/pkg/api/v1"
 	"math/rand"
@@ -31,15 +32,30 @@ const BENCHMARK_NAMESPACE = "benchmark"
 
 func (this CoCoBenchmark) Run() {
 	glog.Info("Running CocoBenchmark...")
-	this.createPod()
+	this.createPod(fmt.Sprintf("test-cpuspin-%d", rand.Uint32()),
+		"icgog/cpuspin:dev", "Turtle", "900m", "128Mi",
+		[]string{"/bin/sh", "-c", "/tmp/cpu_spin 60"})
+	this.createPod(fmt.Sprintf("test-memstream-1m-%d", rand.Uint32()),
+		"icgog/memstream:dev", "Rabbit", "900m", "128Mi",
+		[]string{"/bin/sh", "-c", "/tmp/mem_stream 1048576"})
+	this.createPod(fmt.Sprintf("test-memstream-50m-%d", rand.Uint32()),
+		"icgog/memstream:dev", "Devil", "900m", "128Mi",
+		[]string{"/bin/sh", "-c", "/tmp/mem_stream 52428800"})
+	this.createPod(fmt.Sprintf("test-iostream-write-%d", rand.Uint32()),
+		"icgog/iostream:dev", "Devil", "400m", "4096Mi",
+		[]string{"/bin/sh", "-c", "/usr/bin/fio /tmp/fio-seqwrite.fio"})
+	this.createPod(fmt.Sprintf("test-iostream-read-%d", rand.Uint32()),
+		"icgog/iostream:dev", "Devil", "400m", "4096Mi",
+		[]string{"/bin/sh", "-c", "/usr/bin/fio /tmp/fio-seqread.fio"})
+	//	this.createPod(fmt.Sprintf("test-nginx-pod-%d", rand.Uint32()), "nginx:latest", "Sheep")
 }
 
-func (this CoCoBenchmark) createPod() {
-	name := fmt.Sprintf("test-nginx-pod-%d", rand.Uint32())
+func (this CoCoBenchmark) createPod(name, image, tasktype, requestCPU, requestMem string, cmd []string) {
 	annots := make(map[string]string)
 	annots["scheduler.alpha.kubernetes.io/name"] = "poseidon"
 	labels := make(map[string]string)
 	labels["scheduler"] = "poseidon"
+	labels["taskType"] = tasktype
 	_, err := this.Clientset.Pods(BENCHMARK_NAMESPACE).Create(&v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        name,
@@ -48,15 +64,21 @@ func (this CoCoBenchmark) createPod() {
 		},
 		Spec: v1.PodSpec{
 			Containers: []v1.Container{{
-				Name:            fmt.Sprintf("container-%s", name),
-				Image:           "nginx:latest",
+				Name:            name,
+				Image:           image,
 				ImagePullPolicy: "IfNotPresent",
+				Resources: v1.ResourceRequirements{
+					Requests: v1.ResourceList{
+						v1.ResourceName(v1.ResourceCPU):    resource.MustParse(requestCPU),
+						v1.ResourceName(v1.ResourceMemory): resource.MustParse(requestMem),
+					}},
+				Command: cmd,
 			}}},
 	})
 	if err != nil {
 		panic(err)
 	}
-	time.Sleep(time.Duration(20 * time.Second))
+	time.Sleep(time.Duration(80 * time.Second))
 }
 
 func (this CoCoBenchmark) Setup() {
